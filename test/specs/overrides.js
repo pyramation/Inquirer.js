dedfa4db68d349a28cccd91934efe3531713c048
Dan Lynch <pyramation@gmail.com>
2017-08-25 15:25:32 +0200

    first attempt, but breaks some tests

---
+++ test/specs/overrides.js
@@ -0,0 +1,40 @@
+/**
+ * Inquirer public API test
+ */
+
+var expect = require('chai').expect;
+var inquirer = require('../../lib/inquirer');
+var autosubmit = require('../helpers/events').autosubmit;
+
+describe('inquirer overrides', function () {
+  beforeEach(function () {
+    this.prompt = inquirer.createPromptModule({here: 'are some options'});
+  });
+
+  it('should allow user to specify overrides', function () {
+    var overrides = {
+      q1: false
+    };
+    var prompts = [
+      {
+        type: 'confirm',
+        name: 'q1',
+        message: 'message',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'q2',
+        message: 'message',
+        default: false
+      }
+    ];
+    var promise = this.prompt(prompts, overrides);
+    autosubmit(promise.ui);
+
+    return promise.then(function (answers) {
+      expect(answers.q1).to.be.false;
+      expect(answers.q2).to.be.false;
+    });
+  });
+});
